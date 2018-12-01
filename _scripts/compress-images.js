53160ce3742a1f52fa53b4dfe6def85a0200e50e
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2018-12-01 22:45:25 +0100

    Compress jpgs in documents/, too

--- _scripts/compress-images.js
+++ _scripts/compress-images.js
@@ -5,23 +5,20 @@ const imagemin = require("imagemin");
 const imageminMozjpeg = require("imagemin-mozjpeg");
 const fs = require("fs");
 
-/*
-jimp.read(pathLib.join(process.cwd(), "/img/soeborggruppe-gruppefoto-2018.jpg"))
-    .then((img) => img.clone())
-    .then((img) => img.resize(400, jimp.AUTO))
-    .then((img) => img.writeAsync("img/soeborggruppe-gruppefoto-2018-resized.jpg"))
-    .then(() => imagemin([ "img/soeborggruppe-gruppefoto-2018-resized.jpg" ], {
-        plugins: [ imageminMozjpeg({ quality: 95 }) ]
-    }))
-    .then(async (result) => fs.writeFileSync("img/soeborggruppe-gruppefoto-2018-resized.jpg", result[0].data))
-    .catch((error) => console.error("Something went wrong", error));
-*/
+(async function() {
+    await resizeAndCompressImages("photos/**/*.jpg");
+    await resizeAndCompressImages("documents/**/*.jpg")
+})();
 
-findAllPhotos().then(resizePhotos).then(compressPhotos);
+async function resizeAndCompressImages(glob) {
+    let paths = await findUnresizedImagesMatchingGlob(glob);
+    let resizedImagePaths = await resizePhotos(paths);
+    await compressPhotos(resizedImagePaths);
+}
 
-async function findAllPhotos() {
-    let pathNames = await fastGlob("photos/**/*.jpg", { absolute: true });
-    return pathNames.filter((pathName) => !pathName.match(/\-resized\-[0-9]+.jpg$/));
+async function findUnresizedImagesMatchingGlob(glob) {
+    let pathNames = await fastGlob(glob, { absolute: true });
+    return pathNames.filter((pathName) => !pathName.match(/\-resized\-[0-9]+\.jpg$/));
 }
 
 async function resizePhotos(paths) {
@@ -51,32 +48,26 @@ async function resizePhotos(paths) {
 }
 
 async function compressPhotos(paths) {
+    process.stdout.write("Compressing images...\r");
+
     let total = paths.length;
     let completed = 0;
 
-    process.stdout.write("Compressing images...\r");
-
-    /*let compressedImages = await imagemin(paths, {
-        plugins: [ imageminMozjpeg({ quality: 95 }) ]
-    });*/ // if this does not work, try batching 100 at a time for compression
-    let compressedImages = await imagemin([ "photos/**/*-resized-*.jpg" ], {
-        plugins: [ imageminMozjpeg({ quality: 85 }) ]
-    });
-
-    process.stdout.write(`Compressed ${compressedImages.length} images.\r\n`);
-
-    await Promise.all(compressedImages.map(({ data }, i) => {
-        let path = paths[i];
-        return new Promise((resolve, reject) => {
-            fs.writeFile(path, data, (error) => {
-                if(error) {
-                    return reject(error);
-                }
-                completed++;
-                process.stdout.write(`Saved ${completed}/${total} compressed images.\r`);
-                resolve();
+    await Promise.all(paths.map((path) => {
+        imagemin([ path ], { plugins: [ imageminMozjpeg({ quality: 85 }) ] })
+            .then((compressedImages) => {
+                let { data } = compressedImages[0];
+                return new Promise((resolve, reject) => {
+                    fs.writeFile(path, data, (error) => {
+                        if(error) {
+                            return reject(error);
+                        }
+                        completed++;
+                        process.stdout.write(`Saved ${completed}/${total} compressed images.\r`);
+                        resolve();
+                    });
+                });
             });
-        });
     }));
 
     console.log("\n");
